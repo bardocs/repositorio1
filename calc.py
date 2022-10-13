fdca8f9e4f173cbe63c9561248347464bff64227
Brandon <brandonaraujo018@gmail.com>
2022-10-13 19:43:34 -0300

    Commit inicial da calculadora

---
+++ calc.py
@@ -0,0 +1,23 @@
+
+soma =  int(input("faça sua conta: "))
+soma2 = int(input("faça sua conta: "))
+print("===============================")
+print(f"resultado: {soma + soma}\n")
+
+soma3 =  int(input("faça sua conta: "))
+soma4 =  int(input("faça sua conta: "))
+print("===============================")
+print(f"resultado: {soma3 - soma4}\n")
+
+soma5 =  int(input("faça sua conta: "))
+soma6 =  int(input("faça sua conta: "))
+print("===============================")
+print(f"resultado: {soma5 * soma6}\n")
+
+soma7 =  int(input("faça sua conta: "))
+soma8 =  int(input("faça sua conta: "))
+print("===============================")
+print(f"resultado: {soma7 / soma8}\n")
+
+
+
